3aa88018e9de5881903873a6aaaca2bfb5871d93
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-05-14 01:43:12 +0200

    fix: filled dto test classes with test data

--- src/app/modules/admin/dto/apprentice-creation-dto.spec.ts
+++ src/app/modules/admin/dto/apprentice-creation-dto.spec.ts
@@ -2,6 +2,13 @@ import { ApprenticeCreationDto } from './apprentice-creation-dto';
 
 describe('ApprenticeCreationDto', () => {
   it('should create an instance', () => {
-    expect(new ApprenticeCreationDto()).toBeTruthy();
+    expect(new ApprenticeCreationDto(
+      "Alainna",
+      "Kriss",
+      "elizabeth_gottq0oi@critical.ev",
+      "J6gk1u4sewUcD",
+      0,
+      0
+    )).toBeTruthy();
   });
 });
--- src/app/modules/admin/dto/trainer-creation-dto.spec.ts
+++ src/app/modules/admin/dto/trainer-creation-dto.spec.ts
@@ -2,6 +2,12 @@ import { TrainerCreationDto } from './trainer-creation-dto';
 
 describe('TrainerCreationDto', () => {
   it('should create an instance', () => {
-    expect(new TrainerCreationDto()).toBeTruthy();
+    expect(new TrainerCreationDto(
+      "Alainna",
+      "Kriss",
+      "elizabeth_gottq0oi@critical.ev",
+      "J6gk1u4sewUcD",
+      0
+    )).toBeTruthy();
   });
 });
