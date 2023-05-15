890a2445102526237e1430c297d85afe2114fb27
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-05-15 11:10:45 +0200

    feat(submission component): added call to backend for request submission

--- src/app/modules/exemption-request/components/request-submission/request-submission.component.ts
+++ src/app/modules/exemption-request/components/request-submission/request-submission.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import {SelectOption} from "../../../../shared/components/shared-dropdown/shared-dropdown.component";
 import {ExemptionCategory} from "../../enum/exemption-category";
 import {FormBuilder, Validators} from "@angular/forms";
+import {RequestSubmissionDto} from "../../dto/request-submission-dto";
+import {AuthService} from "../../../authentication/service/auth.service";
+import {lastValueFrom} from "rxjs";
+import {User} from "../../../../shared/models/user";
+import {ExemptionRequestService} from "../../service/exemption-request.service";
+import {NotificationService} from "../../../../core/services/notification.service";
 
 @Component({
   selector: 'app-request-submission',
@@ -21,8 +27,36 @@ export class RequestSubmissionComponent {
   })
 
   constructor(
-    private formBuilder: FormBuilder
+    private authService: AuthService,
+    private exemptionRequestService: ExemptionRequestService,
+    private formBuilder: FormBuilder,
+    private notificationService: NotificationService
   ) { }
+
+  async onSubmit() {
+    const requestSubmission: RequestSubmissionDto = await this.buildRequestSubmissionFromSubmissionForm()
+
+    console.log(requestSubmission)
+    console.log(new Date(requestSubmission.startTime))
+
+    this.exemptionRequestService.submitExemptionRequest(requestSubmission).subscribe({
+      next: () => this.notificationService.showSuccess(`Deine Dienstbefreiung wurde eingereicht`),
+      error: () => this.notificationService.showError("Etwas ist schiefgelaufen. Versuche es später erneut")
+    })
+  }
+
+  private async buildRequestSubmissionFromSubmissionForm(): Promise<RequestSubmissionDto> {
+
+    const submissionFormValues = this.submissionForm.value
+    const requestSubmission: RequestSubmissionDto = Object.assign(new RequestSubmissionDto(), submissionFormValues)
+
+    const currentUser$ = this.authService.getCurrentUser()
+    const currentUser: User = await lastValueFrom(currentUser$)
+
+    requestSubmission.applicantId = currentUser.id
+
+    return requestSubmission
+  }
 
   private getExemptionCategoryDropdownOptions(): SelectOption[] {
     return Object.entries(ExemptionCategory)
