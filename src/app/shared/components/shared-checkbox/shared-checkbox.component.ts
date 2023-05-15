c1ab60c7a8659e18fa86f747ae89b0cde2f1a0f0
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-05-15 08:31:58 +0200

    feat: finished shared-checkbox component

--- src/app/shared/components/shared-checkbox/shared-checkbox.component.ts
+++ src/app/shared/components/shared-checkbox/shared-checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-shared-checkbox',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./shared-checkbox.component.css']
 })
 export class SharedCheckboxComponent {
+
+  @Input()
+  inputId?: string
 
 }
