1bde07afca9e21f4237fb2e2785534dd410aab19
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-06-05 23:35:11 +0200

    feat: added model class of request processing

---
+++ src/app/modules/exemption-request/model/request-processing.spec.ts
@@ -0,0 +1,7 @@
+import { RequestProcessing } from './request-processing';
+
+describe('RequestProcessing', () => {
+  it('should create an instance', () => {
+    expect(new RequestProcessing()).toBeTruthy();
+  });
+});
