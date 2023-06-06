04c8272c6cd0fb616141dd91d34eab1f71ed66cd
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-06-06 23:14:33 +0200

    feat: added api call on clicking the save button for updating a request

--- src/app/modules/exemption-request/components/request-details/request-details.component.ts
+++ src/app/modules/exemption-request/components/request-details/request-details.component.ts
@@ -1,8 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {ExemptionRequest} from "../../model/exemption-request";
-import {Observable} from "rxjs";
+import {Observable, of} from "rxjs";
 import {ExemptionRequestService} from "../../service/exemption-request.service";
+import {
+  RequestDetailsEditingFormComponent
+} from "./request-details-editing-form/request-details-editing-form.component";
+import {NotificationService} from "../../../../core/services/notification.service";
 
 @Component({
   selector: 'app-request-details',
@@ -11,13 +15,17 @@ import {ExemptionRequestService} from "../../service/exemption-request.service";
 })
 export class RequestDetailsComponent implements OnInit {
 
+  @ViewChild(RequestDetailsEditingFormComponent)
+  private editingFormComponent!: RequestDetailsEditingFormComponent
   private exemptionRequestId!: number
+
   exemptionRequest$!: Observable<ExemptionRequest>
   isCurrentlyEditing: boolean = false
 
   constructor(
     private route: ActivatedRoute,
-    private exemptionRequestService: ExemptionRequestService
+    private exemptionRequestService: ExemptionRequestService,
+    private notificationService: NotificationService
   ) { }
 
   ngOnInit(): void {
@@ -32,9 +40,35 @@ export class RequestDetailsComponent implements OnInit {
     return parseInt(<string>routeId)
   }
 
+  attemptExemptionRequestUpdate() {
+
+    const editingForm = this.editingFormComponent.exemptionRequestEditingForm
+
+    if (editingForm.invalid) {
+      editingForm.markAllAsTouched()
+      this.notificationService.showError("Bitte überprüfe deine Eingaben erneut.", "Speichern fehlgeschlagen!")
+      return
+    }
+
+    this.performExemptionRequestUpdate();
+  }
+
+  private performExemptionRequestUpdate() {
+
+    const updatedExemptionRequest: ExemptionRequest = Object.assign(new ExemptionRequest(), this.editingFormComponent.exemptionRequestEditingForm.value)
+
+    this.exemptionRequestService.updateExemptionRequestById(this.exemptionRequestId, updatedExemptionRequest).subscribe({
+      next: value => {
+        this.notificationService.showSuccess("Der Antrag wurde aktualisiert")
+        this.exemptionRequest$ = of(value)
+      },
+      error: () => this.notificationService.showError("Etwas ist schiefgelaufen. Versuche es erneut oder wende dich an den Support")
+    })
+
+  }
+
   toggleEditingState() {
     this.isCurrentlyEditing = !this.isCurrentlyEditing
   }
 
-  protected readonly onabort = onabort;
 }
