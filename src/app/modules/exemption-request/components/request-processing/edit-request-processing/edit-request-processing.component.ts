50ff151575e8ce6f08e11da57feaec1f72573789
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-06-08 21:42:55 +0200

    feat: added modal asking to confirm the withdrawal

--- src/app/modules/exemption-request/components/request-processing/edit-request-processing/edit-request-processing.component.ts
+++ src/app/modules/exemption-request/components/request-processing/edit-request-processing/edit-request-processing.component.ts
@@ -6,6 +6,10 @@ import {FormBuilder, Validators} from "@angular/forms";
 import {ProcessingStatus} from "../../../enum/processing-status";
 import {RequestProcessingService} from "../../../service/request-processing.service";
 import {NotificationService} from "../../../../../core/services/notification.service";
+import {ModalContent} from "../../../../../shared/components/danger-confirmation-modal/modal-content";
+import {lastValueFrom} from "rxjs";
+import {ModalResponse} from "../../../../../shared/enum/modal-response";
+import {ModalService} from "../../../../../shared/service/modal.service";
 
 @Component({
   selector: 'app-edit-request-processing',
@@ -29,7 +33,8 @@ export class EditRequestProcessingComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private requestProcessingService: RequestProcessingService,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    private modalService: ModalService
   ) { }
 
   ngOnInit(): void {
@@ -41,7 +46,19 @@ export class EditRequestProcessingComponent implements OnInit {
     this.editProcessingStatusForm.controls.comment.setValue(this.requestProcessing.comment)
   }
 
-  withdrawRequestProcessing() {
+  async openWithdrawalConfirmationModal() {
+    const modalContent: ModalContent = {
+      title: "Antragsbearbeitung zurückziehen?",
+      content: "Bist du sicher, dass du die Antragsbearbeitung zurückziehen möchtest? Dieser Schritt kann nicht rückgängig gemacht werden."
+    }
+
+    const modalRef = this.modalService.createDangerConfirmationModal(modalContent)
+    const modalResponse = await lastValueFrom(modalRef.closed)
+
+    if (modalResponse === ModalResponse.Confirm) this.withdrawRequestProcessing()
+  }
+
+  private withdrawRequestProcessing() {
 
     this.requestProcessingService.withdrawRequestProcessing(this.requestProcessing.id).subscribe({
       next: () => {
