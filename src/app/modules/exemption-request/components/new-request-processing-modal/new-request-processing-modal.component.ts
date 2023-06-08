be8073d0f2a9617e3ff3cd3fcd8bad5a5d83ffa5
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-06-08 14:44:40 +0200

    feat: modal returns data for new request processing when being closed and submitted

--- src/app/modules/exemption-request/components/new-request-processing-modal/new-request-processing-modal.component.ts
+++ src/app/modules/exemption-request/components/new-request-processing-modal/new-request-processing-modal.component.ts
@@ -1,5 +1,9 @@
 import {Component} from '@angular/core';
 import {DialogRef} from "@angular/cdk/dialog";
+import {SelectOption} from "../../../../shared/components/shared-dropdown/shared-dropdown.component";
+import {ProcessingStatusUtil} from "../../util/processing-status-util";
+import {FormBuilder, Validators} from "@angular/forms";
+import {RequestProcessingUpdateDto} from "../../dto/request-processing-update-dto";
 
 @Component({
   selector: 'app-new-request-processing-modal',
@@ -8,6 +12,24 @@ import {DialogRef} from "@angular/cdk/dialog";
 })
 export class NewRequestProcessingModalComponent {
 
-  constructor(public dialogRef: DialogRef<string>) { }
+  readonly processingStatusDropdownOptions: SelectOption[] = ProcessingStatusUtil.getStatusForNewProcessingAsDropdownOption()
+  readonly requestProcessingForm = this.formBuilder.group({
+    processingStatus: [null, Validators.required],
+    comment: [null]
+  })
+
+  constructor(
+    public dialogRef: DialogRef<string>,
+    private formBuilder: FormBuilder
+  ) { }
+
+  onSubmit() {
+    const requestProcessingUpdateDto: RequestProcessingUpdateDto = Object.assign(new RequestProcessingUpdateDto(), this.requestProcessingForm.value)
+
+    // Needs to be converter to string first, because the dialog.close method can only return string values
+    const dtoAsString = JSON.stringify(requestProcessingUpdateDto)
+
+    this.dialogRef.close(dtoAsString)
+  }
 
 }
