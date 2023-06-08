b3bbd3567968d91d6f0acfd29f63900c57f48141
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-06-08 14:42:13 +0200

    feat: created dto for new request processing

---
+++ src/app/modules/exemption-request/dto/request-processing-update-dto.spec.ts
@@ -0,0 +1,7 @@
+import { RequestProcessingUpdateDto } from './request-processing-update-dto';
+
+describe('RequestProcessingUpdateDto', () => {
+  it('should create an instance', () => {
+    expect(new RequestProcessingUpdateDto()).toBeTruthy();
+  });
+});
