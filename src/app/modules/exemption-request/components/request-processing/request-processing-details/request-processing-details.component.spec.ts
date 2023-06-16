8480b80ffb7a38501c488dfeec004653d1ba6884
Fabian Hintermayr <97839471+fhintermayr@users.noreply.github.com>
2023-06-16 13:01:21 +0200

    test: provided missing jwt for IfRoleDirektive

--- src/app/modules/exemption-request/components/request-processing/request-processing-details/request-processing-details.component.spec.ts
+++ src/app/modules/exemption-request/components/request-processing/request-processing-details/request-processing-details.component.spec.ts
@@ -7,17 +7,26 @@ import {CardComponent} from "../../../../../shared/components/card/card.componen
 import {CardHeaderComponent} from "../../../../../shared/components/card/card-header/card-header.component";
 import {SharedButtonComponent} from "../../../../../shared/components/shared-button/shared-button.component";
 import {IfRoleDirective} from "../../../../../shared/directives/if-role.directive";
+import {AuthService} from "../../../../authentication/service/auth.service";
 
 describe('RequestProcessingDetailsComponent', () => {
   let component: RequestProcessingDetailsComponent;
   let fixture: ComponentFixture<RequestProcessingDetailsComponent>;
+
+  const authServiceMock = {
+    getToken: () => 'dummy-token', // Dummy-Token zurückgeben
+    getUsersRole: () => 'dummy-role' // Dummy-Rolle zurückgeben
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
         RequestProcessingDetailsComponent, CardComponent, CardHeaderComponent, SharedButtonComponent, IfRoleDirective
       ],
-      imports: [ HttpClientTestingModule, ToastrModule.forRoot() ]
+      imports: [ HttpClientTestingModule, ToastrModule.forRoot() ],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock } // AuthService Mock bereitstellen
+      ]
     });
     fixture = TestBed.createComponent(RequestProcessingDetailsComponent);
     component = fixture.componentInstance;
